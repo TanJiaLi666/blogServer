6cbb26d682c19ca0bb65b4b3435c737ff6c548c3
hasee <2331988047@qq.com>
2022-10-18 19:53:20 +0800

    ##前端系统初始化（100%）

---
+++ blogFront/src/test/java/com/tanjiali/front/BlogFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanjiali.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
